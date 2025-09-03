e96079c6624c742155921cafea70b0eb29e634ac
Juampy-19 <jpcastellano1990@gmail.com>
2025-09-03 15:34:16 -0300

    Creación del archivo connection.js para la conexion a la db

---
+++ app/api/movies/route.js
@@ -0,0 +1,10 @@
+import { pool } from '@/db/connection';
+
+export async function GET() {
+    try {
+        const [rows] = await pool.query('SELECT * FROM movies');
+        return new Response(JSON.stringify(rows), { status: 200 });
+    } catch (error) {
+        return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+    }
+}
