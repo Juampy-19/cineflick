50b23b5dddf21212c96d4f3aa174a6a6eafe3077
Juampy-19 <jpcastellano1990@gmail.com>
2025-09-08 15:46:39 -0300

    modificación en el archivo route.js

--- app/api/movies/route.js
+++ app/api/movies/route.js
@@ -3,7 +3,12 @@ import { pool } from '@/db/connection';
 export async function GET() {
     try {
         const [rows] = await pool.query('SELECT * FROM movies');
-        return new Response(JSON.stringify(rows), { status: 200 });
+        return new Response(JSON.stringify(rows), {
+            status: 200,
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
     } catch (error) {
         return new Response(JSON.stringify({ error: error.message }), { status: 500 });
     }
