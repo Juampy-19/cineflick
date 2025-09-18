8a519f8bee4b47ae84f29c90b228d8eb8e407bc7
Juan Pablo Castellano <jpcastellano1990@gmail.com>
2025-09-18 14:57:04 -0300

    Diseño de la vista register

--- app/register/page.js
+++ app/register/page.js
@@ -1,5 +1,56 @@
 export default function RegisterPage() {
     return (
-        <h1>Pagina de registro</h1>
+        <form>
+            <div className="flex flex-col items-center gap-15 p-6 m-20">
+                <div className="w-xl flex items-center justify-between">
+                    <label>Nombre</label>
+                    <input
+                        type="name"
+                        name="name"
+                        placeholder="Nombre"
+                        className="w-xs"
+                    />
+                </div>
+                <div className="w-xl flex items-center justify-between">
+                    <label>Apellido</label>
+                    <input
+                        type="lastname"
+                        name="lastname"
+                        placeholder="Apellido"
+                        className="w-xs"
+                    />
+                </div>
+                <div className="w-xl flex items-center justify-between">
+                    <label>Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        className="w-xs"
+                    />
+                </div>
+                <div className="w-xl flex items-center justify-between">
+                    <label>Contraseña</label>
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Contraseña"
+                        className="w-xs"
+                    />
+                </div>
+                <div className="w-xl flex items-center justify-between">
+                    <label>Confirmar contraseña</label>
+                    <input
+                        type="password"
+                        name="confirmPassword"
+                        placeholder="Confirmar contraseña"
+                        className="w-xs"
+                    />
+                </div>
+                <div>
+                    <button>Crear cuenta</button>
+                </div>
+            </div>
+        </form>
     )
 }
