231b8418f42018263f2ec91a0537fcf35be4584f
Juan Pablo Castellano <jpcastellano1990@gmail.com>
2025-09-30 15:35:01 -0300

    Implementación de pathname para marcar el link activo en el header

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,31 +1,38 @@
+'use client';
+
 import Link from "next/link";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+    const pathname = usePathname();
+
+    const linkClass = (path) => `hover:text-[var(--green)]  transition-colors ${pathname === path ? 'border-b-2 border-[var(--green)]' : '' }`;
+
     return (
-        <header className="w-full ">
-            <div className="flex items-center justify-evenly">
-                <Image 
-                    src="/img/cineflick-logo.svg" 
+        <header>
+            <div className="flex items-center justify-between">
+                <Image
+                    src="/img/cineflick-logo.svg"
                     alt="Logo de CineFlick"
                     width={150}
                     height={150}
                 />
-                <h1>CineFlick</h1>
+                <nav className="text-xl flex items-center justify-center gap-8">
+                    <Link href='/' className={linkClass('/')}>Inicio</Link>
+                    <Link href='/api/movies' className={linkClass('api/movies')}>Cartelera</Link>
+                    <Link href='/candy' className={linkClass('/candy')}>Candy</Link>
+                    <Link href='/login' className={linkClass('/login')}>
+                        <FontAwesomeIcon icon={faArrowRightToBracket}
+                            width={20}
+                            className="hover:text-[var(--green)] transition-colors"
+                        />
+                    </Link>
+                </nav>
+                <h1 className="p-6 text-2xl">CineFlick</h1>
             </div>
-            <nav className="flex items-center justify-center gap-4">
-                <Link href='/' className="hover:text-[var(--green)] transition-colors">Inicio</Link>
-                <Link href='/api/movies' className="hover:text-[var(--green)] transition-colors">Cartelera</Link>
-                <Link href='/candy' className="hover:text-[var(--green)] transition-colors">Candy</Link>
-                <Link href='/login'>
-                    <FontAwesomeIcon icon={faArrowRightToBracket} 
-                        width={20}
-                        className="hover:text-[var(--green)] transition-colors"
-                    />
-                </Link>
-            </nav>
         </header>
     )
 }
