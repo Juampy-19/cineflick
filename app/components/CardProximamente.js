df22caa7c79c0ee7114654220878258a69186fa2
Juan Pablo Castellano <jpcastellano1990@gmail.com>
2025-10-15 15:30:12 -0300

    Modificación de estilos en el componente CardProximamente.js

--- app/components/CardProximamente.js
+++ app/components/CardProximamente.js
@@ -49,20 +49,37 @@ export default function Card() {
         return <p>No hay películas disponibles</p>
     }
 
+    const classificationColor = (classification) => {
+        switch(classification) {
+            case 1:
+                return 'bg-green-500';
+            case 2:
+                return 'bg-blue-500';
+            case 3:
+                return 'bg-yellow-500';
+            default:
+                return 'bg-gray-500';
+        }
+    };
+
     return (
-        <div className="relative w-full px-10">
+        <div className="relative w-2/3 px-10">
             <div ref={carrouselRef} id="carrousel" className="flex overflow-hidden space-x-6 pb-4">
                 {proximamente.map((movie) => (
-                    <div key={movie.id} className="flex-shrink-0 w-56 flex flex-col items-center border-2 border-[var(--green)] rounded-xl bg-black">
-                        <h2 className="mt-5 text-sm text-center p-2 h-10 flex items-center justify-center">{movie.title}</h2>
-                        <div className="w-full h-full items-center justify-center overflow-hidden p-4">
+                    <div key={movie.id} className="relative flex-shrink-0 w-56 flex flex-col items-center border-2 border-[var(--green)] rounded-xl shadow-lg">
+                        <div className="w-full h-full items-center justify-center overflow-hidden">
                             <img
                                 src={movie.poster_url}
                                 alt="Poster de la película"
                                 className="w-full h-full rounded-xl"
                             />
                         </div>
-                        <p className="mb-5">{movie.classification}</p>
+                        <h2 className="mt-5 text-sm text-center p-2 h-10 flex items-center justify-center">{movie.title}</h2>
+                        <div className="flex items-center justify-center gap-5">
+                            <p className={`mt-3 mb-3 text-black px-2 py-1 rounded-full text-xs font-bold ${classificationColor(movie.classification_id)}`}>{movie.classification}</p>
+                            <p>{movie.release_date}</p>                            
+                        </div>
+                        <p className="absolute right-[0px] bg-black/50 p-1 text-sm rounded-tr-lg rounded-bl-lg">{movie.duration}</p>
                     </div>
                 ))}
             </div>
