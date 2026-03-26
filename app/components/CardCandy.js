3d670ab9ba0ae1489c4e8f0715fd2c1f70f0b46a
Juan Pablo Castellano <jpcastellano1990@gmail.com>
2026-03-26 15:21:12 -0300

    Creación del componente CardCandy.js

---
+++ app/components/CardCandy.js
@@ -0,0 +1,19 @@
+'use client';
+
+export default function CardCandy({ items }) {
+    return (
+        <div>
+            {items.map((item) => (
+                <div key={item.id}>
+                    <h2>{item.title}</h2>
+                    <img
+                        src={item.img}
+                        alt="Imagen del producto."
+                    />
+                    <span>{items.description}</span>
+                    <p>{item.price}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
