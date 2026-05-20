9c5adf0da6ef0eb641eedb7ebda3eaccc34bc0ed
Juan Pablo Castellano <jpcastellano1990@gmail.com>
2026-05-20 14:01:04 -0300

    Se Modificó el diseño del componente CardStore.js

--- app/components/CardStore.js
+++ app/components/CardStore.js
@@ -2,18 +2,19 @@
 
 export default function CardStore({ products }) {
     return (
-        <div className="grid grid-cols-2 md:grid-cols-4 p-4 gap-6">
+        <div className="grid grid-cols-2 md:grid-cols-4 p-4 gap-8">
             {products.map((product) => (
-                <div key={product.id} className="border-2 border-[var(--green)]">
-                    <div>
+                <div key={product.id} className="flex flex-col border-2 border-[var(--green)] bg-[var(--teal)] rounded-xl hover:scale-105 transition-transform duration-300">
+                    <div className="w-full h-50 p-4 overflow-hidden">
                         <img 
                             src={product.img}
                             alt="Imagen del producto"
+                            className="w-full h-full object-contain"
                         />
                     </div>
-                    <h3>{product.title}</h3>
-                    <p>{product.description}</p>
-                    <h5>{product.price}</h5>
+                    <h3 className="text-center p-2 h-[48px] mb-10 md:mb-5">{product.title}</h3>
+                    <span className="text-sm p-2">{product.description}</span>
+                    <h5 className="text-center p-2 text-[var(--green)] mt-auto">${product.price}</h5>
                 </div>
             ))}
         </div>
