c6c05857a4c90fcffc863c59281b9a8c568a3d40
Juan Pablo Castellano <jpcastellano1990@gmail.com>
2026-08-06 15:46:02 -0300

    Creación de la vista admin/store/create para agregar un nuevo producto al store.

---
+++ app/admin/store/create/page.js
@@ -0,0 +1,58 @@
+'use client';
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import ProductForm from "@/app/components/ProductForm";
+import Link from "next/link";
+
+export default function CreateCandyPage() {
+    const router = useRouter();
+    const [store, setStore] = useState({
+        title: '',
+        description: '',
+        image: null,
+        type_id: '',
+        price: ''
+    });
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const formData = new FormData();
+
+        formData.append('title', store.title);
+        formData.append('description', store.description);
+        formData.append('type_id', store.type_id);
+        formData.append('price', store.price);
+        if (store.image) {
+            formData.append('image', store.image);
+        }
+
+        const response = await fetch('/api/store', {
+            method: 'POST',
+            body: formData
+        });
+
+        if (response.ok) {
+            router.push('/admin/store');
+        }
+    }
+
+    return (
+        <div className="p-4 flex flex-col gap-4">
+            <h1 className="text-center text-3xl font-bold">Nuevo producto</h1>
+
+            <ProductForm
+                product={store}
+                setProduct={setStore}
+                onSubmit={handleSubmit}
+                buttonText="Crear producto"
+                typesApiUrl="/api/storeTypes"
+            />
+
+            <Link href={'/admin/store'} className="m-auto">
+                <button className="btn">Volver</button>
+            </Link>
+        </div>
+    )
+}
