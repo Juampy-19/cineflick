e52086a1227834c8f4b11c0d46f75a5ef7fa45bd
Juan Pablo Castellano <jpcastellano1990@gmail.com>
2026-08-06 15:28:31 -0300

    Se implementó en metodo POST en api/store para la creación de un nuevo producto.

--- app/api/store/route.js
+++ app/api/store/route.js
@@ -20,3 +20,86 @@ export async function GET() {
         return new Response(JSON.stringify({ error: error.message }), { status: 500 });
     }
 }
+
+export async function POST(request) {
+    const connection = await pool.getConnection();
+
+    try {
+        const data = await request.formData();
+
+        const title = data.get('title');
+        const description = data.get('description');
+        const image = data.get('image');
+        const type_id = data.get('type_id');
+        const price = data.get('price');
+
+        let img = null;
+
+        if (image && image.size > 0) {
+            const bytes = await image.arrayBuffer();
+            const buffer = Buffer.from(bytes);
+
+            const fileName = `${Date.now()}-${image.name.replace(/\s+/g, '-').toLowerCase()}`;
+
+            const filePath = path.join(
+                process.cwd(),
+                'public',
+                'img',
+                'store',
+                fileName
+            );
+
+            await writeFile(filePath, buffer);
+
+            img = `/img/store/${fileName}`;
+        }
+
+        await connection.beginTransaction();
+
+        const [result] = await connection.query(
+            `
+                INSERT INTO store
+                (
+                    title,
+                    description,
+                    img,
+                    type_id,
+                    price
+                )
+                VALUES (?, ?, ?, ?, ?)
+            `,
+            [
+                title,
+                description,
+                img,
+                type_id,
+                price
+            ]
+        );
+
+        const storeId = result.insertId
+
+        await connection.commit();
+
+        return Response.json(
+            {
+                message: 'Producto creado correctamente',
+                storeId
+            },
+            {
+                status: 201
+            }
+        );
+    } catch (error) {
+        await connection.rollback();
+
+        console.error(error);
+
+        return Response.json(
+            { error: error.message },
+            { status: 500 }
+        );
+    } finally {
+        connection.release();
+    }
+}
